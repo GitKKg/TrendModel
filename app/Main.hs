b64346a57afb474b38da17c53f9a95f24574a131
GitKKg <lancexk@gmail.com>
2020-09-24 16:59:19 +0800

    use Snap.serveDirectory to set html directory

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Control.Concurrent      (forkIO)
+import           Control.Exception       (finally)
+import           Control.Monad           (forever, unless)
+import qualified Data.ByteString         as B
+import qualified Data.ByteString.Char8   as BC
+import qualified Network.WebSockets      as WS
+import qualified Network.WebSockets.Snap as WS
+import           Snap.Core               (Snap)
+import qualified Snap.Core               as Snap
+import qualified Snap.Http.Server        as Snap
+import qualified Snap.Util.FileServe     as Snap
+import qualified System.IO               as IO
+--import qualified System.Process          as Process
+
+app :: Snap ()
+app = Snap.route
+  [
+    -- use Snap.serveDirectory to set html directory
+    ("", Snap.serveDirectory "/home/kyle/WebProgramming/Stock_app/dist/spa-mat/")
+  ]
 
 main :: IO ()
-main = someFunc
+main = Snap.httpServe config app
+  where
+    config =
+        Snap.setErrorLog  Snap.ConfigNoLog $
+        Snap.setAccessLog Snap.ConfigNoLog $
+        Snap.defaultConfig
